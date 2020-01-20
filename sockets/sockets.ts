39cd99921f914ff66ec1f62c8c2eab2436be8876
Raymond Quintero <rquintero@grupocomeca.com>
2020-01-19 23:27:19 -0600

    Socket server

---
+++ sockets/sockets.ts
@@ -0,0 +1,27 @@
+import { Socket } from 'socket.io';
+import socketIO from 'socket.io';
+
+
+export const desconectar = ( cliente: Socket ) => {
+
+    cliente.on('disconnect', () => {
+
+        console.log('Cliente desconectado');
+
+    });
+
+};
+
+
+// ESCUCHAR MENSAJES
+export const mensaje = (cliente: Socket, io: socketIO.Server) => {
+
+    cliente.on('mensaje', ( payload: { de: string, cuerpo: string } , callback) => {
+
+        console.log('Mensaje recibido', payload);
+
+        io.emit('mensaje-nuevo', payload );
+
+    } );
+
+};
